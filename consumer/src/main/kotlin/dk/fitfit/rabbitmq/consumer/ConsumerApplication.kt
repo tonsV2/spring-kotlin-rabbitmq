1396a2c0c6b9a55f8dbe895d34a563b0127a7fc7
sj <sebastianthegreatful@gmail.com>
2018-08-24 17:14:59 +0200

    Add Spring producer and consumer

--- consumer/src/main/kotlin/dk/fitfit/rabbitmq/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/dk/fitfit/rabbitmq/consumer/ConsumerApplication.kt
@@ -1,11 +1,23 @@
 package dk.fitfit.rabbitmq.consumer
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler
+import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class ConsumerApplication
 
 fun main(args: Array<String>) {
     runApplication<ConsumerApplication>(*args)
+}
+
+@Service
+@RabbitListener(queues = ["hello"])
+class RabbitReceiver {
+    @RabbitHandler
+    fun receive(name: String) {
+        println("Received: '$name'")
+    }
 }
